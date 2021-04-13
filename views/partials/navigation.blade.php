61ca06d97c511112c62a37becf9c6cb392403061
tecnodesignc <marcos21.009@gmail.com>
2021-04-13 17:21:41 -0500

    Correction webpackConfig

--- views/partials/navigation.blade.php
+++ views/partials/navigation.blade.php
@@ -1,3 +1,45 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="{{Theme::url('img/logo.png')}}" alt="@setting('core::site-name')" class="logo-header">
+            </a>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            @menu('main',bootstrap-5)
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+
+
+
 <nav class="navbar navbar-default @guest navbar-fixed-top @endguest" style="@auth margin-top: -52px @endauth">
     <div class="container">
         <div class="navbar-header">
@@ -9,7 +51,7 @@
             <a class="navbar-brand" href="{{ URL::to('/') }}">@setting('core::site-name')</a>
         </div>
         <div class="navbar-collapse collapse navbar-responsive-collapse">
-            @menu('main')
+
         </div>
     </div>
 </nav>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const WebpackShellPlugin = require('webpack-shell-plugin');
 const themeInfo = require('./theme.json');
 
 /*
